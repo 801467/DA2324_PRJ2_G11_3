4cb58576243254aac29e96532ff2f032afa98ca5
Tiago Loureiro <19423709+tiagoicp@users.noreply.github.com>
2024-05-15 07:36:48 +0100

    added vertexIndex

--- classes/TSPGraph.cpp
+++ classes/TSPGraph.cpp
@@ -4,5 +4,6 @@ bool TSPGraph::addVertex(int id, double longitude, double latitude) {
     if (findVertex(id) != nullptr)
         return false;
     vertexSet.push_back(new Vertex<int>(id,longitude,latitude));
+    vertexIndex[id] = vertexSet.size() - 1;
     return true;
 }
